329f23c491d8972829df8614208bd2913e48ac75
SBriscoe2 <shabris@tiscali.co.uk>
2018-11-21 15:14:02 +0000

    amended button text and moved add title form into index page

--- app.rb
+++ app.rb
@@ -11,9 +11,9 @@ class BookmarkManager < Sinatra::Base
     erb(:"bookmarks/index")
   end
 
-  get '/bookmarks/new' do
-    erb(:"bookmarks/new")
-  end
+  # get '/bookmarks/new' do
+  #   erb(:"bookmarks/new")
+  # end
 
   post '/bookmarks' do
     Bookmark.create(url: params[:url], title: params[:title])
--- spec/features/create_bookmark_title_spec.rb
+++ spec/features/create_bookmark_title_spec.rb
@@ -1,6 +1,6 @@
 feature 'Add Title to bookmark' do
   scenario ' A user can add a title to a bookmark' do
-    visit('bookmarks/new')
+    visit('/bookmarks')
     fill_in('url', with: 'http://www.testbookmark.com')
     fill_in('title', with: 'Test Bookmark')
     click_button('Submit')
